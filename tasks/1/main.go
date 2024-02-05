dabbfb92db227f17c8a1b56aeb46bc72d03c22d5
Artyom_PC <lilapank@yandex.ru>
2024-02-05 13:10:34 +0300

    add: task 1

--- tasks/1/main.go
+++ tasks/1/main.go
@@ -1,5 +1,61 @@
 package main
 
-func main() {
+import "fmt"
 
+// Структура Human с полями, представляющими человека
+type Human struct {
+	lastName  string
+	firstName string
+	age       uint8
+	growth    uint8
+	gender    string
+}
+
+// Action - структура, встраивающая (наследующая) Human
+type Action struct {
+	// Встраивание Human в Action позволяет использовать его поля и методы
+	Human
+	count int
+}
+
+// Метод для Human
+func (h Human) introduceYourself() {
+	fmt.Printf("Hi, my name is %s %s. I'm %d years old. I am %s and my height is %d cm\n",
+		h.firstName, h.lastName, h.age, h.gender, h.growth)
+}
+
+// Метод для Action
+func (a Action) sayHello() {
+	/*
+		В следующей строчке можно заметить, что мы явно не указываем структуру Human
+		для получения lastName и firstName, так как мы встроили (унаследовали) эти поля
+		от родительской структуры Human. Однако мы можем явно указать a.Human.firstName,
+		это будет необходимо, если в структуре Action есть поля с таким же именем,
+		и мы хотим обратиться конкретно к a.Human.firstName.
+	*/
+	myName := fmt.Sprintf("%s %s", a.firstName, a.lastName)
+	for i := 0; i < a.count; i++ {
+		fmt.Printf("Hi, my name is %s.\n", myName)
+	}
+}
+
+func main() {
+	act := Action{
+		Human: Human{
+			firstName: "Artem",
+			lastName:  "Echeistov",
+			age:       25,
+			growth:    170,
+			gender:    "male",
+		},
+		count: 2,
+	}
+	/*
+		Мы можем не указывать явно Human при вызове метода introduceYourself(),
+		так как структура Human встроена в структуру Action (Action унаследовала методы от Human).
+		В языке Go нам не нужно явно указывать, что Action наследует методы от Human,
+		это делается благодаря встраиванию структуры Human в Action.
+	*/
+	act.introduceYourself()
+	act.sayHello()
 }
