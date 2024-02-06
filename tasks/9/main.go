0da45fae06a732c082d2850e654684d662a40f95
Artyom_PC <lilapank@yandex.ru>
2024-02-06 20:49:41 +0300

    add: task 9

--- tasks/9/main.go
+++ tasks/9/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+
+func woker(input chan int) <-chan int {
+	output := make(chan int)
+	go func() {
+		for value := range input {
+			output <- value * 2
+		}
+		close(output)
+		fmt.Println("workr end")
+	}()
+	return output
+}
+
+func writer(ouput <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for result := range ouput {
+		fmt.Println(result)
+	}
+	fmt.Println("writer end")
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 15, 16, 100, 20, 1, 23, 41, 41, 4, 11, 1, 41, 0}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	input := make(chan int)
+	ouput := woker(input)
+	go writer(ouput, &wg)
+	for _, num := range arr {
+		input <- num
+	}
+	fmt.Println("sender end")
+	close(input)
+	wg.Wait()
 }
