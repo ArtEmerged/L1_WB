802c682a9109d95c313e294dff1240b6e3037a48
Artyom_PC <lilapank@yandex.ru>
2024-02-06 16:13:23 +0300

    add: task 25

--- tasks/25/main.go
+++ tasks/25/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	t := time.Now()
+	sleep(time.Millisecond * 2)
+	fmt.Println(time.Since(t))
+}
+
+func sleep(n time.Duration) {
+	start := time.Now()
+	for time.Since(start) < n {
+	}
 }
