5dfc5c7be97d35acd23bb7cc9e7223ec0bbc3c01
Artyom_PC <lilapank@yandex.ru>
2024-02-06 16:29:35 +0300

    add: task 13

--- tasks/13/main.go
+++ tasks/13/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	fmt.Print("Веедите первое число a=")
+	fmt.Scan(&a)
+	fmt.Print("Веедите второе число b=")
+	fmt.Scan(&b)
+	// выполняем операцию параллельного присваивания
+	a, b = b, a
+	fmt.Printf("a=%d b=%d\n", a, b)
 }
