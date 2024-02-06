0ee334615498960aa6d27907c7e5b36bbff386e9
Artyom_PC <lilapank@yandex.ru>
2024-02-06 15:46:07 +0300

    add: task 20

--- tasks/20/main.go
+++ tasks/20/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	arg := os.Args[1:]
+	if len(arg) != 1 {
+		fmt.Println("incorrect input")
+		return
+	}
+
+	words := string(arg[0])
+	if len(words) == 0 {
+		return
+	}
+	var newWords string
+	if len(words) > 1 {
+		words += " "
+		var begW int
+
+		for i := range words {
+			if words[i] == ' ' {
+				newWords = " " + words[begW:i] + newWords
+				begW = i + 1
+			}
+		}
+	}
+	fmt.Printf("|%s| - |%s|\n", arg[0], newWords[1:])
 }
