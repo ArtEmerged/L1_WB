5421ec6b4a1f965703821d2276b6add5e4cfa486
Artyom_PC <lilapank@yandex.ru>
2024-02-06 11:18:23 +0300

    add: task 3

--- tasks/3/main.go
+++ tasks/3/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{2, 4, 6, 8, 10}
+	out := sqrtIn(arr)
+	var res int
+	for range arr {
+		res += <-out
+	}
+	fmt.Println(res)
+}
+
+func sqrtIn(in []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, num := range in {
+			out <- num * num
+		}
+		close(out)
+	}()
+	return out
 }
