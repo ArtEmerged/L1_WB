ec9620f1127b7761c6331da9a162ef461ade1d08
Artyom_PC <lilapank@yandex.ru>
2024-02-06 14:43:33 +0300

    add: task 19

--- tasks/19/main.go
+++ tasks/19/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	arg := os.Args[1:]
+	if len(arg) != 1 {
+		fmt.Println("incorrect input")
+		return
+	}
+
+	word := []rune(arg[0])
+	if len(word) == 0 {
+		return
+	}
+	if len(word) > 1 {
+		for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+			word[i], word[j] = word[j], word[i]
+		}
+	}
+	fmt.Printf("%s - %s\n", arg[0], string(word))
 }
