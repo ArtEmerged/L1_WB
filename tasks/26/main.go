7ead7dd4aaddc5e5e94cd08babda3028fd657c0d
Artyom_PC <lilapank@yandex.ru>
2024-02-06 17:34:03 +0300

    add: task 26

--- tasks/26/main.go
+++ tasks/26/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Print("введите слово:")
+	var input string
+	fmt.Scan(&input)
+	onlyLow := strings.ToLower(input)
+	counter := make(map[rune]struct{})
+	for _, v := range onlyLow {
+		if _, ok := counter[v]; ok {
+			fmt.Printf("%s - %v\n", input, false)
+			return
+		}
+		counter[v] = struct{}{}
+	}
+	fmt.Printf("%s - %v\n", input, true)
 }
