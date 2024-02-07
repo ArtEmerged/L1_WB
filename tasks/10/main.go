100394ac461d38fd808f5776e575cb1d14fac5c6
Artyom_PC <lilapank@yandex.ru>
2024-02-07 13:12:18 +0300

    add: task 10

--- tasks/10/main.go
+++ tasks/10/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	arr := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	group := map[int][]float64{}
+
+	for _, tem := range arr {
+		key := int(tem) / 10 * 10
+		group[key] = append(group[key], tem)
+	}
+	fmt.Println(group)
 }
