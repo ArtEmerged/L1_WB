6ea7dfbddda5db8befe553fa1496cbc75e48784c
Artyom_PC <lilapank@yandex.ru>
2024-02-07 16:31:44 +0300

    add: task 11

--- tasks/11/main.go
+++ tasks/11/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	set1 := []string{"apple", "banana", "orange", "kiwi", "strawberry", "grapefruit", "pineapple"}
+	set2 := []string{"banana", "grape", "watermelon", "apple", "pear", "peach", "orange"}
+
+	set := setIntersection(set1, set2)
+
+	//apple banana orange
+	fmt.Println(set)
+}
+
+func setIntersection(s1, s2 []string) []string {
+	setMap := make(map[string]struct{}, len(s1))
+	setSlice := []string{}
+
+	for _, value := range s1 {
+		setMap[value] = struct{}{}
+	}
+
+	for _, value := range s2 {
+		if _, ok := setMap[value]; ok {
+			setSlice = append(setSlice, value)
+		}
+	}
+
+	return setSlice
 }
