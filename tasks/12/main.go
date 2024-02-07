d6018fbe028ba9b8c8f91093c690ef0cc887124d
Artyom_PC <lilapank@yandex.ru>
2024-02-07 13:53:18 +0300

    add: task 12

--- tasks/12/main.go
+++ tasks/12/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	s := []string{"cat", "cat", "dog", "cat", "tree"}
+	set := toSet(s)
+	fmt.Println(set)
+}
+
+func toSet(s []string) []string {
+	setMap := make(map[string]struct{})
+	setSlice := []string{}
+	for _, value := range s {
+		if _, ok := setMap[value]; !ok {
+			setMap[value] = struct{}{}
+			setSlice = append(setSlice, value)
+		}
+	}
+	return setSlice
 }
