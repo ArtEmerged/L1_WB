75574cb450da1d61d9c23b775aa26980b799938a
Artyom_PC <lilapank@yandex.ru>
2024-02-08 13:16:16 +0300

    add: task 22

--- tasks/22/main.go
+++ tasks/22/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
+func main() {
+	a, _ := new(big.Int).SetString("110000000000000000000", 10) // 11e+19
+	b, _ := new(big.Int).SetString("10000000000000000000", 10)  // 10e+18
+	var operator big.Int
+	fmt.Println("a + b =", operator.Add(a, b))
+	fmt.Println("a - b =", operator.Sub(a, b))
+	fmt.Println("a / b =", operator.Div(a, b))
+	fmt.Println("a * b =", operator.Mul(a, b))
 }
