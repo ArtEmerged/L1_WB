1f43e7cbf88f7d965e6abb51e05d1c9ec2655dd9
Artyom_PC <lilapank@yandex.ru>
2024-02-08 11:06:50 +0300

    add: task 18

--- tasks/18/main.go
+++ tasks/18/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+type counter struct {
+	num int
+	mu  sync.Mutex
+}
+
+func (c *counter) intrepret(wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	c.num++
+
+}
+
+func main() {
+	counter := counter{}
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go counter.intrepret(&wg)
+	}
+	wg.Wait()
+	fmt.Println(counter.num)
 }
