244c1269a8ac99baa17f19210aa7847f6a3d34c7
Artyom_PC <lilapank@yandex.ru>
2024-02-09 12:45:29 +0300

    add: task 24

--- tasks/24/main.go
+++ tasks/24/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"wblone/tasks/24/point"
+)
+
 func main() {
+	p1 := point.NewPoint(10.4, 2.4)
+	p2 := point.NewPoint(1.1, 8.4)
+	distance := p1.Distance(p2)
+	fmt.Printf("Расстояние между точками a%.2f и b%.2f = %.3f\n", p1, p2, distance)
 
 }
