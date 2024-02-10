0a25a3222ad29c06805873c9fdc830a1c9a08829
Artyom_PC <lilapank@yandex.ru>
2024-02-10 17:00:01 +0300

    add: comment to task 25

--- tasks/25/main.go
+++ tasks/25/main.go
@@ -5,14 +5,29 @@ import (
 	"time"
 )
 
+/*
+	Реализовать собственную функцию sleep.
+*/
+
 func main() {
+	// Определяем текущее время для замера
 	t := time.Now()
-	sleep(time.Millisecond * 2)
+
+	// Вызываем функцию sleep на 2 секунды (2000 миллисекунд)
+	sleep(time.Millisecond * 2000)
+
+	// Выводим результат замера
 	fmt.Println(time.Since(t))
 }
 
 func sleep(n time.Duration) {
+	// Определяем текущее время
 	start := time.Now()
+	/*
+		time.Since(start) возвращает, сколько прошло времени с момента start (time.Now.Sub(start)).
+		Цикл будет продолжаться до тех пор, пока время, прошедшее с момента обновления переменной start, меньше n (переданное время для sleep)
+	*/
 	for time.Since(start) < n {
+		// Пустой цикл ожидания
 	}
 }
