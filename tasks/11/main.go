c5456a49d4d87d2ebb5a7f091d3ffd111c6f6aa5
Artyom_PC <lilapank@yandex.ru>
2024-02-12 16:18:57 +0300

    add: comment to task 11

--- tasks/11/main.go
+++ tasks/11/main.go
@@ -2,25 +2,39 @@ package main
 
 import "fmt"
 
+/*
+	Реализовать пересечение двух неупорядоченных множеств.
+*/
+
 func main() {
 	set1 := []string{"apple", "banana", "orange", "kiwi", "strawberry", "grapefruit", "pineapple"}
 	set2 := []string{"banana", "grape", "watermelon", "apple", "pear", "peach", "orange"}
 
 	set := setIntersection(set1, set2)
 
-	//apple banana orange
+	//вывод:apple banana orange
 	fmt.Println(set)
 }
 
+// Функция setIntersection возвращает пересечение двух неупорядоченных множеств
 func setIntersection(s1, s2 []string) []string {
+	/*
+		Инициализруем map для хранения уникальных значений, в качестве значения используем пустую структуру,
+		так как она занимает 0 байт
+	*/
 	setMap := make(map[string]struct{}, len(s1))
+
+	// Инициализация среза для хранения уникальных элементов пересечения множеств
 	setSlice := []string{}
 
+	//	Проходимя циклом по элементов первого среза и добавляем их в map
 	for _, value := range s1 {
 		setMap[value] = struct{}{}
 	}
 
+	//	Проходимя циклом по элементов второго среза
 	for _, value := range s2 {
+		// Если элемент присутствует в map, добавляем его в срез пересечений множеств
 		if _, ok := setMap[value]; ok {
 			setSlice = append(setSlice, value)
 		}
