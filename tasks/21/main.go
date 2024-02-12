dd9465bbe858770cbfe35500b57141352a544faa
Artyom_PC <lilapank@yandex.ru>
2024-02-12 18:57:49 +0300

    add: task 21

--- tasks/21/main.go
+++ tasks/21/main.go
@@ -1,5 +1,42 @@
 package main
 
+/*
+	Реализовать паттерн «адаптер» на любом примере.
+*/
+
+// Интерфейс, к которому мы адаптируем нашу структуру.
+type Target interface {
+	Process()
+}
+
+// Несовместимуая структура
+type Adaptee struct {
+}
+
+// Метод, который необходимо адаптировать
+func (a *Adaptee) AdapteeProcess() {
+}
+
+// Структура Адаптер
+type Adapter struct {
+	*Adaptee
+}
+
+// Конструктор адаптера
+func NewAdapter(adaptee *Adaptee) Target {
+	return &Adapter{adaptee}
+}
+
+// Вызывает адаптированный метод AdapteeProcess
+func (a *Adapter) Process() {
+	a.AdapteeProcess()
+}
+
 func main() {
+
+	// Создаем адаптер
+	adapter := NewAdapter(&Adaptee{})
+
+	adapter.Process()
 
 }
