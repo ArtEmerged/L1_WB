9beae041e73f0cd5ff408567150b8767777884ce
Artyom_PC <lilapank@yandex.ru>
2024-02-12 14:33:18 +0300

    add: comment to task 22

--- tasks/22/main.go
+++ tasks/22/main.go
@@ -5,12 +5,28 @@ import (
 	"math/big"
 )
 
+/*
+	Разработать программу, которая перемножает, делит, складывает,
+	вычитает две числовых переменных a, b, значение которых > 2^20.
+*/
+
 func main() {
-	a, _ := new(big.Int).SetString("110000000000000000000", 10) // 11e+19
-	b, _ := new(big.Int).SetString("10000000000000000000", 10)  // 10e+18
+	/*
+		Инициализируем переменные a и b с помощью пакета big.Int для работы с большими числами.
+		Значение a устанавливается как 11e+19, значение b устанавливается как 10e+18
+	*/
+	a, _ := new(big.Int).SetString("110000000000000000000", 10)
+
+	b, _ := new(big.Int).SetString("10000000000000000000", 10)
+
+	// Инициализация переменной operator типа big.Int для выполнения операций
 	var operator big.Int
+
 	fmt.Println("a + b =", operator.Add(a, b))
+
 	fmt.Println("a - b =", operator.Sub(a, b))
+
 	fmt.Println("a / b =", operator.Div(a, b))
+
 	fmt.Println("a * b =", operator.Mul(a, b))
 }
